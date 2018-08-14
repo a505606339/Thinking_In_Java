8d8157f0ad5e56a276edec6a0c23113a891f780e
a505606339 <505606339@qq.com>
2018-08-14 19:24:13 +0800

    第二章学习内容

---
+++ src/sayHello/Print.java
@@ -0,0 +1,26 @@
+package sayHello;
+
+import java.util.*;
+
+/** The first Thinking in java example program.
+ * Displays a string and today`s date.
+ * @author HP
+ * @version 4.0
+ *
+ */
+public class Print {
+	/** 这是类的主方法入口
+	 * @param args
+	 * @throws 并不抛出异常
+	 */
+	public static void main(String[] args) {
+		System.out.print("Hello, it`s:");
+		System.out.println(new Date());
+		System.out.println("a string of things");
+		
+	}
+} 
+/* output:
+Hello, it`s:Fri Aug 10 19:01:54 CST 2018
+a string of things
+*///:~
